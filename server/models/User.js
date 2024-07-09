dc8475c7939e8cbf55bccc3d600867015e4425ae
Jordan M <jormmiller@gmail.com>
2024-07-08 21:17:51 -0400

    fix models

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,29 @@
 const { Schema, model} = require('mongoose');
+const App = require('./App');
 
-const userAppSchema = new Schema({
-  app_id: { type: Schema.Types.ObjectId, ref: 'App', required: true },
-  purchase_date: { type: Date, required: true },
-});
+const transactionAppsSchema = new Schema({
+  app: { type: Schema.Types.ObjectId, ref: 'App', required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+}, { _id: false });
+
+const userTransactionSchema = new Schema({
+  transaction_date: { type: Date, required: true },
+  total: { type: Number, required: true },
+  apps: [transactionAppsSchema],
+}, { _id: false });
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  library: [userAppSchema] // Embedded document
+  library: [{ type: Schema.Types.ObjectId, ref: 'App' }],
+  transactions: [userTransactionSchema],
+});
+
+userSchema.pre('remove', function(next) {
+  App.deleteMany({ _id: { $in: this.library } })
+    .then(() => next());
 });
 
 const User = model('User', userSchema);
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const App = require('./App');
+const User = require('./User');
+
+module.exports = { App, User };
