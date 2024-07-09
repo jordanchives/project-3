8206afaa30c19f4b8b891485f89e29d73271fc42
Jordan M <jormmiller@gmail.com>
2024-07-08 20:25:50 -0400

    server fix

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const startApolloServer = async () => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use('/graphql', expressMiddleware(server));
-    app.unsubscribe(express.static(path.join(__dirname, '../client/dist/')));
+    app.use(express.static(path.join(__dirname, '../client/dist/')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/dist/index.html'));
