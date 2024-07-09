9581864badaa6dce454dbc5988146f84b2055b60
Jordan M <jormmiller@gmail.com>
2024-07-09 19:26:37 -0400

    password encryption

--- server/models/User.js
+++ server/models/User.js
@@ -1,29 +1,48 @@
-const { Schema, model} = require('mongoose');
-const App = require('./Game');
+const bcrypt = require("bcrypt");
+const { Schema, model } = require("mongoose");
+const Game = require("./Game");
 
-const transactionGamesSchema = new Schema({
-  game: { type: Schema.Types.ObjectId, ref: 'App', required: true },
-  price: { type: Number, required: true },
-}, { _id: false });
+const transactionGamesSchema = new Schema(
+  {
+    game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
+    price: { type: Number, required: true },
+  },
+  { _id: false }
+);
 
-const userTransactionSchema = new Schema({
-  transaction_date: { type: Date, required: true },
-  total: { type: Number, required: true },
-  games: [transactionGamesSchema],
-}, { _id: false });
+const userTransactionSchema = new Schema(
+  {
+    transaction_date: { type: Date, required: true },
+    total: { type: Number, required: true },
+    games: [transactionGamesSchema],
+  },
+  { _id: false }
+);
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  library: [{ type: Schema.Types.ObjectId, ref: 'App' }],
+  library: [{ type: Schema.Types.ObjectId, ref: "Game" }],
   transactions: [userTransactionSchema],
 });
 
-userSchema.pre('remove', function(next) {
-  App.deleteMany({ _id: { $in: this.library } })
-    .then(() => next());
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  try {
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+})
+
+userSchema.pre("remove", function (next) {
+  Game.deleteMany({ _id: { $in: this.library } }).then(() => next());
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 module.exports = User;
