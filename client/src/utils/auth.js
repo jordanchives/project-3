d90ec02559fbdd9b2c2bfd9269eb9cbae0ce646f
Jordan M <jormmiller@gmail.com>
2024-07-09 22:41:24 -0400

    ghetto login implementation

---
+++ client/src/utils/auth.js
@@ -0,0 +1,42 @@
+import { jwtDecode } from "jwt-decode";
+
+class AuthService {
+  getProfile() {
+    return jwtDecode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = jwtDecode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem("id_token");
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.reload();
+  }
+}
+
+const auth = new AuthService();
+
+export default auth;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require('./utils/auth');
 const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const exp = require("constants");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,6 +17,7 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
     await server.start();
+    app.use(cors());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use('/graphql', expressMiddleware(server, {context: authMiddleware}));
