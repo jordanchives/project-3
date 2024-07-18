864476e50aaa5efcc48be493c80a310b0b501291
Connor Colvill <cnnrclvll@gmail.com>
2024-07-18 18:58:39 -0400

    login

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -29,15 +29,15 @@ function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen m-2">
-      <div className="w-full max-w-md bg-[#222] rounded-xl shadow-md py-8 px-8">
-        <h2 className="text-[25px] font-bold text-white mb-6 text-center">
-          Login
+    <div className="flex flex-col items-center h-screen m-8">
+      <div className="w-md bg-[#151624] rounded-xl shadow-md py-8 px-8">
+        <h2 className="form-text mb-6 text-center">
+          login
         </h2>
         <form action="" className="flex flex-col" onSubmit={handleFormSubmit}>
           <input
             placeholder="Username"
-            className="bg-gray-700 text-white border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none transition ease-in-out duration-150 placeholder-gray-300"
+            className="bg-[#D6C9AE] text-[#151624] border-0 rounded-md p-2 mb-4 focus:bg-[#F2A007] focus:outline-none transition ease-in-out duration-150 placeholder-[#151624]"
             type="text"
             name="username"
             id="username"
@@ -45,24 +45,24 @@ function Login() {
 
           <input
             placeholder="Password"
-            className="bg-gray-700 text-white border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none transition ease-in-out duration-150 placeholder-gray-300"
+            className="bg-[#D6C9AE] text-[#151624] border-0 rounded-md p-2 mb-4 focus:bg-[#F2A007] focus:outline-none transition ease-in-out duration-150 placeholder-[#151624]"
             type="password"
             id="password"
           />
           <span
-            style={{ color: "white" }}
+            style={{ color: "#D6C9AE" }}
             className={`${visible ? "invisible" : ""} mt-4 text-center pb-4`}
           >
             {" "}
             Invalid Login Credentials!
           </span>
           <button
-            className="bg-gradient-to-r from-indigo-500 to-blue-500 rounded-full text-white font-medium py-2 px-4 hover:bg-indigo-600 hover:to-blue-600 transtion ease-in duration-200"
+            className="bg-gradient-to-r from-[#F2A007] to-[#f0560f] rounded-full text-white font-medium py-2 px-4 hover:bg-[#f0560f] hover:to-[#F2A007] transtion ease-in duration-200"
             type="submit"
           >
             Submit
           </button>
-          <p className="text-white mt-4 text-center">
+          <p className="text-[#D6C9AE] mt-4 text-center">
             Don't have an account?
             <a href="/register" className="text-white-500 hover:underline mt-4 px-1 ">
               Register
